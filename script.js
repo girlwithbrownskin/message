4f426f70133ed9b860547895b0468aa526c991e4
girlwithbrownskin <swapshanuprabha@gmail.com>
2025-02-24 14:24:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 14th February,love!";
+  question.innerHTML = "Yay, see you on the 1st April,love!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
