efe48528a8fab731a0d55a9573f7406f4d4beafe
girlwithbrownskin <swapshanuprabha@gmail.com>
2026-02-09 00:57:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const noBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yay,now eat me out!!";
   gif.src =
-    "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTk3YTF0M242cmZ1bjkxZG1ocDhxeW10c3pwbzgzcWhjc3ozanJ3MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/eij0RsEQ7XVP6lollt/giphy.giff";
+    "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTk3YTF0M242cmZ1bjkxZG1ocDhxeW10c3pwbzgzcWhjc3ozanJ3MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/eij0RsEQ7XVP6lollt/giphy.gif";
 });
 
 noBtn.addEventListener("mouseover", () => {
